c2adf27299d75631143468d2b98720d03b9576e3
Abdullah Jawad <abdullahjawad.2005@gmail.com>
2025-09-28 17:28:47 +0500

    func for middle node

---
+++ LinkedList/getMiddleNode.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+Node* getMiddleNode(Node* head){
+    if (head==nullptr) return nullptr;
+
+    Node* slow=head;
+    Node* fast=head;
+    while (fast!=nullptr && fast->next!=nullptr){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
