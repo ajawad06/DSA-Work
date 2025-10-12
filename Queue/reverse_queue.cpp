a04891c138d921c662a24653fe2220d9d7a7e08a
Abdullah Jawad <abdullahjawad.2005@gmail.com>
2025-10-12 20:01:50 +0500

    Reverse Queue

---
+++ Queue/reverse_queue.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void reverseQueue(queue<int>& q){
+    if (q.empty()){
+        return;
+    }
+    int front=q.front();
+    q.pop();
+    reverseQueue(q);
+    q.push(front);
+}
+
+int main() {
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+
+    reverseQueue(q);
+
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    // Output: 3 2 1
+}
