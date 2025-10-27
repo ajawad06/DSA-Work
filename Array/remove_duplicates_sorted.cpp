3aacbd3324ac8528afd8bffdd27c0e00d4e76492
Abdullah Jawad <abdullahjawad.2005@gmail.com>
2025-10-27 22:27:17 +0500

    remove duplicates in sorted array

--- Array/remove_duplicates_sorted.cpp
+++ Array/remove_duplicates_sorted.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 int remove_duplicates(int arr[],int size){
-    int unique=0;
-    for (int i=0;i<size;i++){
-        unique^=arr[i];
+    int j=0;
+    for (int i=1;i<size;i++){
+        if (arr[i]!=arr[j]){ // if unique element found
+            j++;             // increase the count of unique 
+            arr[j]=arr[i];   // move it next to last unique
+        }
     }
-    return unique;
+    return j+1; // new size
 }
 
 
